6249c9be785ad01edf65284d81adea3b97e56742
mattp532 <pmatt.212@gmail.com>
2026-03-10 11:24:25 -0400

    add comments

--- backend/routes/report_routes.py
+++ backend/routes/report_routes.py
@@ -1,3 +1,17 @@
+'''
+routes for handling report operations
+
+notice how we dependency inject the db/session into the handler
+
+this is going to help us when we create unit tests for use cases
+
+and we don't want to use a real db!!! we can use a mock
+
+db service which is faster and standard practice for unit tests
+
+router gives real db to service, but unit test file will give fake db to service
+'''
+
 from fastAPI import APIRouter, Depends
 from ..services.report_service import create_report
 from ..schemas import ReportCreate, ReportResponse
@@ -6,7 +20,7 @@ from sqlalchemy.orm import Session
 router = APIRouter(prefix="/reports")
 
 @router.post("/create", response_model=ReportResponse)
-async def create_report(report_create: ReportCreate, session: Session = Depends(get_db)):
-    new_report = create_report(report_create, session)
+async def create_report(report_create: ReportCreate, db: Session = Depends(get_db)):
+    new_report = create_report(report_create, db)
     return new_report
 
