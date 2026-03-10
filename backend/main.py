0273bb783db42d348301b403ce7e2b78c90d6e44
mattp532 <pmatt.212@gmail.com>
2026-03-09 23:57:31 -0400

    init server and add requirements

--- backend/main.py
+++ backend/main.py
@@ -1,24 +1,7 @@
-'''
-Yay!!!
-don't you love OOP
+from fastapi import FastAPI
 
-we also need FastAPI
-'''
+app = FastAPI()
 
-from database import DB
-from datamodels import Report, User
-
-database = DB()
-
-# How to use!
-
-report_to_add = Report(name="test", description="test description", importance=0)
-# database.add_bug(report_to_add)
-# database.rem_bug("test")
-
-results = database.get_all_reports()
-
-for report in results:
-    print(f"ID: {report.id}, Name: {report.name}, Description: {report.description}, Importance: {report.importance}")
-
-database.close()
+@app.get("/")
+async def root():
+    return {"Message": "Hello Wilson"}
