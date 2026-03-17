12c832be74a27d20d495172276a6cbc03173c814
mattp532 <pmatt.212@gmail.com>
2026-03-17 13:27:25 -0400

    add user pydantic models

---
+++ backend/schemas/user_schema.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+
+#Request Models
+class UserLogin(BaseModel):
+    login: str #email or username
+    password: str
+
+class UserRegister(BaseModel):
+    username: str
+    email: str
+    password: str
+
+#Helper model to help nest info into object field
+class UserInfo(BaseModel):
+    id: int
+    username: str
+    email: str
+
+#Response Model
+class UserResponse(BaseModel):
+    user: UserInfo
+    access_token: str
+    token_type: str
+    
