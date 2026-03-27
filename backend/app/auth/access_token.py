485f0bd04285c18496931d8fa69e2bd0d4ff29c4
mattp532 <pmatt.212@gmail.com>
2026-03-27 12:22:21 -0400

    add comment

--- backend/app/auth/access_token.py
+++ backend/app/auth/access_token.py
@@ -5,6 +5,10 @@ create and verify jwt
 SECRET = "looooongsecretttjdsahposaiodsaoudsaoi2918321873721398hjhjshjshdjsa"
 
 def create_access_token(olddata: dict):
+    """
+    Receive a dictionary with user id or anything else we wanna put in jwt
+    payload, encode the jwt then return it as jwt string
+    """
     data = olddata.copy()
     data["exp"] = 6942067
     return jwt.encode(data, SECRET, "HS256")
