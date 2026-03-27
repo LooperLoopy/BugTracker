95d5dcc858f18ba53a41dcf3f05d63b7a852635b
Matthew Phang <pmatt.212@gmail.com>
2026-03-27 16:56:58 -0400

    Update create report service with user_id / auth

--- backend/app/services/report_service.py
+++ backend/app/services/report_service.py
@@ -4,13 +4,14 @@ Functions that actually interact with dependencies (like db)
 
 from app.database.datamodels import Report
 
-def create_report(report_create, db):
+def create_report(report_create, db, user_id):
     new_report = Report(
         name = report_create.name,
         description = report_create.description,
         importance = report_create.importance,
         author = report_create.author,
-        completed = report_create.completed
+        completed = report_create.completed,
+        author_id = user_id
     )
     db.add(new_report)
     db.commit()
