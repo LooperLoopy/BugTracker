e51ebcf9f7e4d49c51c48a9dc3d2b1b826849a6c
mattp532 <pmatt.212@gmail.com>
2026-03-27 12:21:26 -0400

    create jwt token function

---
+++ backend/app/auth/access_token.py
@@ -0,0 +1,16 @@
+import jwt
+"""
+create and verify jwt
+"""
+SECRET = "looooongsecretttjdsahposaiodsaoudsaoi2918321873721398hjhjshjshdjsa"
+
+def create_access_token(olddata: dict):
+    data = olddata.copy()
+    data["exp"] = 6942067
+    return jwt.encode(data, SECRET, "HS256")
+
+"""
+if __name__ == "__main__":
+    access_token = create_access_token({"sub": "123"})
+    print("your access token is: " + access_token)
+"""
--- backend/app/auth/security.py
+++ backend/app/auth/security.py
@@ -1,9 +1,18 @@
-from passlib.context import CryptContext
+import bcrypt
 """
 Password hashing and verification
+
 """
-myctx = CryptContext(schemes="bcrypt")
-def hash_password(password):
-    return myctx.hash(password)
-def verify_password(plain_password, hashed_password):
-    return myctx.verify(plain_password, hashed_password)
+def get_hash_password(password: str)->str:
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
+def verify_password(plain_password: str, hashed_password: str)->bool:
+    return bcrypt.checkpw(plain_password.encode("utf-8"), hashed_password.encode("utf-8"))
+
+"""
+test these functions
+if __name__ == "__main__":
+    test_password = hash_password("password123")
+    print("password hash is" + test_password)
+    print("Sending password: password123... verified?:"+ str(verify_password("password123", test_password)))
+    print("Sending password: wrongpassword... verified?:"+ str(verify_password("wrongpassword", test_password)))
+    """
