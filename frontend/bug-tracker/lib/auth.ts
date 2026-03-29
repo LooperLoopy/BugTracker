5af6c472df14a6b21137015beae6bfae912f5231
Matthew Phang <matthew.phang@torontomu.ca>
2026-03-28 23:02:28 -0400

    auth functions frontend

--- frontend/bug-tracker/lib/auth.ts
+++ frontend/bug-tracker/lib/auth.ts
@@ -1,1 +1,36 @@
+import { stringify } from "querystring"
+
 //Functions like signup / login and token stuff
+const BASE_URL = "http://localhost:8000"
+export async function signup(email: string, username: string, password: string){
+    const res = await fetch(`${BASE_URL}/user/signup`,{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            email: email,
+            username: username,
+            password: password 
+        })
+
+
+    })
+    if (!res.ok) throw new Error("Signup failed")
+    return res.json()
+}
+
+export async function login(login: string, password: string){
+    const res = await fetch(`${BASE_URL}/user/login`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            login: login,
+            password: password
+            })
+    })
+    if (!res.ok) throw new Error("Login failed")
+    return res.json()
+}
